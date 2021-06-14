f23b13977b0455ad923caea2ef1aba72b8e5fa5d
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-14 11:40:55 +0200

    created components for shopping cart and cart total

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import './Header.css';
+import './CartItems.css'
+import './CartTotal.css'
 import Header from './Header.js'
+import AppMain from './AppMain.js'
 
 function App() {
   return (
     <div className="App">
       <Header title="Amazon Cart" />
+      <AppMain />
     </div>
   );
 }
---
+++ src/AppMain.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AppMain() {
+  return (
+    <div className="App-main">
+        <div className="CartItems">
+          <h1>Shopping Cart</h1>
+        </div>
+        <div className="CartTotal">
+
+        </div>
+      </div>
+  )
+}
+
+export default AppMain
