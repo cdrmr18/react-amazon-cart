a340d7d776095bb005af725c90f457d4e31e03c6
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-17 22:15:48 +0200

    added data file and added useState in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React, { useState }  from 'react'
 import './App.css';
-import Header from './components/Header.js'
-import AppMain from './components/AppMain.js'
+import Header from './components/Header'
+import AppMain from './components/AppMain'
+import data from './Data'
 
 function App() {
+  const [ cartItems, setCartItems ] = useState(data);
+ console.log(cartItems)
   return (
     <div className="App">
       <Header title="Amazon Cart" />
