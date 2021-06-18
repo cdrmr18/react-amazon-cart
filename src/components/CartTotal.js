b6a1bf8a7a0e76004c577ec7ead52860960c32ca
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-18 23:33:07 +0200

    started working on cart total companonent html and styling

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -4,7 +4,15 @@ import './CartTotal.css'
 function CartTotal() {
     return (
         <div className="CartTotal">
-          Cart Total
+          <h3>
+            Subtotal(5 items):
+            <span className="CartTotal-price">
+              $2,899.00
+            </span>
+          </h3>
+          <button>
+            Proceed to checkout
+          </button>
         </div>
     )
 }
